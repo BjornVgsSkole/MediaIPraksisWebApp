4a616a3cdea25682b514d83ebe022942f9b8b11a
Bjorn VGS Skole <bjornvgsskole@gmail.com>
2025-05-15 20:37:38 +0200

    First code version

---
+++ app.js
@@ -0,0 +1,32 @@
+// Import Express.js module
+import express from 'express';
+import projectsModule from './projects.js';
+
+import path from 'path';
+const __dirname = path.resolve();
+
+// Initialize the Express application
+const app = express();
+
+// Define the port number for the server to listen on
+const port = parseInt(process.env.PORT) || 8080;
+
+// Define a route for the root URL ('/') and specify the response
+app.get('/', (req, res) => {
+  res.send('Hello, World!'); // Send 'Hello, World!' as the response
+});
+
+// Send HTML file to web browser for the URL ('/index')
+app.get('/index', function(req, res) {
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+app.get('/getAllProjects', async function(req, res) {
+    let retVal = await projectsModule.GetAllProjects("test");
+    res.json({ success: true, message: "OK", projects: retVal.projects });
+});
+
+// Start the server and have it listen on the defined port
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`); // Log a message to the console indicating the server is running
+});
---
+++ projects.js
@@ -0,0 +1,72 @@
+import sqlite from 'sqlite3';
+const dbPath = './database.db'; // Path to the database file
+
+export default {
+    
+    async GetAllProjects(param) {
+        // Connect to the database in a physical file
+        const db = new sqlite.Database(dbPath);
+        let arrayProjects = [];
+        
+        // Database operations
+        //db.serialize(() => {
+            // Create a table
+            //db.run("CREATE TABLE lorem (info TEXT)");
+
+            // Insert data into the table
+            //const stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+            //for (let i = 0; i < 10; i++) {
+            //    stmt.run("Ipsum " + i);
+            //}
+            //stmt.finalize();
+
+            let rows = await db_all("select projectId, clientId, clientName, title, workDescription, \
+                                     qualifications, contactEmail, contactPhone, estimatedWorkdays, \
+                                     statusId, dateCreated from projects order by projectId", db);
+            for(var item in rows){
+                arrayProjects.push({ projectId: rows[item].projectId, clientName: rows[item].clientName,
+                                     title: rows[item].title, workDescription: rows[item].workDescription,
+                                     qualifications: rows[item].qualifications, contactEmail: rows[item].contactEmail,
+                                     statusId: rows[item].statusId, dateCreated: rows[item].dateCreated,
+                                     estimatedWorkdays: rows[item].estimatedWorkdays
+                 });    
+            }
+            return {success: true, message: "OK", projects: arrayProjects};
+            
+            // Query data from the table
+            /*db.all("SELECT rowid AS id, info FROM lorem", (err, rows) => {
+                //if (err) reject(err);
+                console.log("Rows", rows.length);
+                for(var item in rows){
+                    arrayProjects.push({ id: rows[item].id, name: rows[item].info });    
+                }
+                return {success: true, message: "OK", data: arrayProjects};
+            });*/
+            
+            
+            //let dbResult = db.each("SELECT rowid AS id, info FROM lorem", (err, row) => {
+            //    arrayProjects.push({ id: row.id, name: row.info });
+                //console.log("arrayProjects2b", arrayProjects);
+                //console.log(row.id + ": " + row.info);
+            //});
+            //console.log("dbResult");
+
+            //console.log("arrayProjects2", arrayProjects);
+            //return {success: true, message: "OK", data: arrayProjects};
+        //});
+
+        // Close the connection when you're done
+        db.close();
+        
+        console.log("DbClosed");
+    } 
+}
+
+async function db_all(query, db){
+    return new Promise(function(resolve,reject){
+        db.all(query, function(err,rows){
+           if(err){return reject(err);}
+           resolve(rows);
+         });
+    });
+}
